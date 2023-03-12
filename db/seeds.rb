021710445e993adb70353da649b768048b506746
Bernard065 <bebenibernard@gmail.com>
2023-03-13 00:29:51 +0300

    create a new Student model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "Bernard", last_name: "Bebeni", grade: 95)
+Student.create(first_name: "Victoria", last_name: "Muya", grade: 75)
+Student.create(first_name: "Samuel", last_name: "Arambe", grade: 60)
+Student.create(first_name: "Tabby", last_name: "Dangu", grade: 56)
